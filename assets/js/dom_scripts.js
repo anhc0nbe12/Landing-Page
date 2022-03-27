a62cc5910ed400a61891a9feccfafb5d82a942c6
anhc0nbe12 <tvh12123@gmail.com>
2022-03-27 11:31:44 +0700

    finish project

---
+++ assets/js/dom_scripts.js
@@ -0,0 +1,44 @@
+const toggler_tablet_nav = document.querySelector(".navbar-tablet-list")
+const bar = document.querySelector(".fa-bars")
+
+
+bar.addEventListener('click', () => {
+   toggler_tablet_nav.classList.toggle('d-block')
+})
+
+
+const navbar_tablet_2 = document.querySelectorAll(".navbar-tablet-list-heading")
+navbar_tablet_2.forEach((item) => {
+   item.addEventListener('click', () => {
+      item.nextElementSibling.classList.toggle('d-block')
+      if (item.nextElementSibling.classList.contains('d-block')){
+         item.childNodes[3].classList.remove('fa-plus')
+         item.childNodes[3].classList.add('fa-minus')
+      } else {
+         item.childNodes[3].classList.remove('fa-minus')
+         item.childNodes[3].classList.add('fa-plus')
+      }
+   })
+})
+
+const trigger_navbar_3 = document.querySelectorAll(".navbar-tablet-list-child-heading")
+
+
+
+
+trigger_navbar_3.forEach((item) => {
+   if(!item.nextElementSibling){
+      item.childNodes[3].style.display = 'none'
+   }
+
+   item.addEventListener('click', ()=>{
+      item.nextElementSibling?.classList.toggle('d-block')
+      if (item.nextElementSibling.classList.contains('d-block')){
+         item.childNodes[3].classList.remove('fa-plus')
+         item.childNodes[3].classList.add('fa-minus')
+      } else {
+         item.childNodes[3].classList.remove('fa-minus')
+         item.childNodes[3].classList.add('fa-plus')
+      }
+   })
+})
